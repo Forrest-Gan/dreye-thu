302243ea4e36bccd126f15d01f5823ffbcc59f7d
AaronGe <aaron@AaronGedeMacBook-Pro.local>
2019-10-11 12:47:54 +0800

    for Tensorflow 2.0

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# dreye-thu
- dreye on tensorflow 2.0
+### Disclaimer
+A new version of the code is available in the final repository of the DR(eye)VE project. Thank you to [Andrea Palazzi](https://github.com/ndrplz) and [Davide Abati](https://github.com/DavideA) for wrapping it up: https://github.com/ndrplz/dreyeve.
+Modified by GE Yulong @ THU
+
+# Where should you attend while driving?
+![alt text](https://raw.githubusercontent.com/cvpr17-id2919/driving/master/misc/img.png "Sample Image")
+
+## System requirements
+The code was tested with the following configuration:
+  * Macbook Pro 2017 4 Thurder 3 
+  * Python 3.6
+  * Tensorflow 2.0
+  
+## How to run
+Just download the repo and start `predict.py`. Together with code, we release pre-trained weights and sample data from the DR(eye)VE dataset to test.
---
+++ utils_NWHC.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from tensorflow.keras.layers import Input, Flatten, concatenate
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Conv3D, MaxPooling3D, Conv2D, UpSampling2D
+from tensorflow.keras.layers import Reshape
+import os
+from os.path import join
+import sys
+import cv2
+import numpy as np
+
+
+# In[2]:
+
+
+# parameters (no need to edit)
+t, c, w, h = 16, 3, 112, 112
+upsample = 4
+
+
+# In[3]:
+
+
+def getCoarse2FineModel_NWHC(summary=True):
+
+    # defined input
+    videoclip_cropped = Input((t, h, w, c), name='input1')
+    videoclip_original = Input((t, h, w, c), name='input2')
+    last_frame_bigger = Input(( h*upsample, w*upsample, c), name='input3')
+  
+
+    # coarse saliency model
+    coarse_saliency_model = Sequential()
+    coarse_saliency_model.add(Conv3D(64, [3, 3, 3], activation='relu', padding='same', name='conv1', strides=(1, 1, 1)))
+    coarse_saliency_model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2), padding='valid', name='pool1'))
+    coarse_saliency_model.add(Conv3D(128, [3, 3, 3], activation='relu', padding='same', name='conv2', strides=(1, 1, 1)))
+    coarse_saliency_model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='valid', name='pool2'))
+    coarse_saliency_model.add(Conv3D(256, [3, 3, 3], activation='relu', padding='same', name='conv3a', strides=(1, 1, 1)))
+    coarse_saliency_model.add(Conv3D(256, [3, 3, 3], activation='relu', padding='same', name='conv3b', strides=(1, 1, 1)))
+    coarse_saliency_model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='valid', name='pool3'))
+    coarse_saliency_model.add(Conv3D(512, [3, 3, 3], activation='relu', padding='same', name='conv4a', strides=(1, 1, 1)))
+    coarse_saliency_model.add(Conv3D(512, [3, 3, 3], activation='relu', padding='same', name='conv4b', strides=(1, 1, 1)))
+    coarse_saliency_model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(4, 2, 2), padding='valid', name='pool4'))
+    coarse_saliency_model.add(Reshape((7, 7, 512)))
+    coarse_saliency_model.add(BatchNormalization())
+    coarse_saliency_model.add(Conv2D(256, [3, 3], kernel_initializer='glorot_uniform', padding='same'))
+    coarse_saliency_model.add(LeakyReLU(alpha=.001))
+    coarse_saliency_model.add(UpSampling2D(size=(2, 2), data_format='channels_last'))
+    coarse_saliency_model.add(BatchNormalization())
+    coarse_saliency_model.add(Conv2D(128, [3, 3], kernel_initializer='glorot_uniform', padding='same'))
+    coarse_saliency_model.add(LeakyReLU(alpha=.001))
+    coarse_saliency_model.add(UpSampling2D(size=(2, 2)))
+    coarse_saliency_model.add(BatchNormalization())
+    coarse_saliency_model.add(Conv2D(64, [3, 3], kernel_initializer='glorot_uniform', padding='same'))
+    coarse_saliency_model.add(LeakyReLU(alpha=.001))
+    coarse_saliency_model.add(UpSampling2D(size=(2, 2), data_format='channels_last'))
+    coarse_saliency_model.add(BatchNormalization())
+    coarse_saliency_model.add(Conv2D(32, [3, 3], kernel_initializer='glorot_uniform', padding='same'))
+    coarse_saliency_model.add(LeakyReLU(alpha=.001))
+    coarse_saliency_model.add(UpSampling2D(size=(2, 2), data_format='channels_last'))
+    coarse_saliency_model.add(BatchNormalization())
+    coarse_saliency_model.add(Conv2D(16, [3, 3], kernel_initializer='glorot_uniform', padding='same'))
+    coarse_saliency_model.add(LeakyReLU(alpha=.001))
+    coarse_saliency_model.add(BatchNormalization())
+    coarse_saliency_model.add(Conv2D(1, [3, 3], kernel_initializer='glorot_uniform', padding='same'))
+    coarse_saliency_model.add(LeakyReLU(alpha=.001))
+
+    # loss on cropped image
+    coarse_saliency_cropped = coarse_saliency_model(videoclip_cropped)
+    cropped_output = Flatten(name='cropped_output')(coarse_saliency_cropped)
+
+    # coarse-to-fine saliency model and loss
+    coarse_saliency_original = coarse_saliency_model(videoclip_original)
+
+    x = UpSampling2D((upsample, upsample), name='coarse_saliency_upsampled')(coarse_saliency_original)  # 112 x 4 = 448
+    
+    x = concatenate([x, last_frame_bigger],axis=3)  # merge the last RGB frame
+
+    x = Conv2D(32, [3, 3], padding='same', kernel_initializer='he_normal')(x)
+    x = Conv2D(64, [3, 3], padding='same', kernel_initializer='he_normal')(x)
+    x = LeakyReLU(alpha=.001)(x)
+    x = Conv2D(32, [3, 3], padding='same', kernel_initializer='he_normal')(x)
+    x = LeakyReLU(alpha=.001)(x)
+    x = Conv2D(32, [3, 3], padding='same', kernel_initializer='he_normal' )(x)
+    x = LeakyReLU(alpha=.001)(x)
+    x = Conv2D(16, [3, 3], padding='same', kernel_initializer='he_normal')(x)
+    x = LeakyReLU(alpha=.001)(x)
+    x = Conv2D(4, [3, 3], padding='same', kernel_initializer='he_normal')(x)
+    x = LeakyReLU(alpha=.001)(x)
+
+    fine_saliency_model = Conv2D(1, [3, 3], padding='same', activation='relu')(x)
+
+    # loss on full image
+    full_fine_output = Flatten(name='full_fine_output')(fine_saliency_model)
+
+    final_model = Model(inputs=[videoclip_cropped, videoclip_original, last_frame_bigger],
+                        outputs=[cropped_output, full_fine_output])
+
+    if summary:
+        print (final_model.summary())
+
+    return final_model
+
+
+# In[4]:
+
+
+def predict_video(model, folder_in, output_path, mean_frame_path):
+
+    # load frames to predict
+    frames = []
+    frame_list = os.listdir(folder_in)
+    mean_frame = cv2.imread(mean_frame_path)
+    print(mean_frame.shape)
+    for frame_name in frame_list:
+        frame = cv2.imread(join(folder_in, frame_name))
+        frames.append(frame.astype(np.float32) - mean_frame)
+    print ('Done loading frames.')
+
+    # start of prediction
+    for i in range(t, len(frames)):
+
+        sys.stdout.write('\r{0}: predicting on frame {1:06d}...'.format(folder_in, i))
+
+        # loading videoclip of t frames
+        x = np.array(frames[i - t: i])
+
+        x_last_bigger = cv2.resize(x[-1, :, :, :], (h*upsample,w*upsample))
+        x_last_bigger = x_last_bigger.transpose(2, 0, 1)
+        x_last_bigger = x_last_bigger[None, :]
+
+        x = np.array([cv2.resize(f, (h, w)) for f in x])
+        x = x[None, :]
+        x = x.transpose(0, 4, 1, 2, 3).astype(np.float32)
+
+        # predict attentional map on last frame of the videoclip
+        res = model.predict_on_batch([x, x, x_last_bigger])
+        res = res[1]  # keep only fine output
+        res = np.clip(res, a_min=0, a_max=255)
+
+        # normalize attentional map between 0 and 1
+        res_norm = ((res / res.max()) * 255).astype(np.uint8)
+        res_norm = np.reshape(res_norm, (h*upsample,w*upsample))
+
+        cv2.imwrite(join(output_path, '{0:06d}.png'.format(i)), res_norm)
+
+
+# In[5]:
+
+
+from utils import getCoarse2FineModel, predict_video
+from tensorflow.keras.optimizers import Adam
+
+if __name__ == '__main__':
+    
+    mean_frame = cv2.imread('data_sample/dreyeve_mean_frame.png')
+    print(mean_frame.shape)
+    
+    output_dir_root = 'out'
+    weights_file = 'weights/model_weights.h5'
+    dreyeve_data_dir = 'data_sample/54'
+
+    # load model for prediction
+    model = getCoarse2FineModel_NWHC(summary=True)
+    opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    model.compile(optimizer=opt,
+                  loss={'cropped_output': 'mse', 'full_fine_output': 'mse'},
+                  loss_weights={'cropped_output': 1.0, 'full_fine_output': 1.0})
+    
+
+    # load pre-trained weights
+    #model.load_weights(weights_file)
+
+    # predict on sample data (first 200 frames of run 54 from DR(eye)VE
+    #predict_video(model, dreyeve_data_dir,
+    #              output_path=output_dir_root,
+    #              mean_frame_path='data_sample/dreyeve_mean_frame.png')
+
+
+# In[ ]:
+
+
+
+
